562e8bf2eb2853df3573bcf561388b58fdc5a050
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-17 16:30:37 +0300

    added client response models

---
+++ distributed/MSA/api-gateway/ApiGateway.Clients.MvcAngular/Models/ApiErrorResponseResult.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiGateway.Clients.MvcAngular.Models
+{
+    public class ApiErrorResponseResult
+    {
+        public string Message { get; set; }
+    }
+}
--- distributed/MSA/api-gateway/ApiGateway.Clients.MvcAngular/Models/SampleValue.cs
+++ distributed/MSA/api-gateway/ApiGateway.Clients.MvcAngular/Models/SampleValue.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiGateway.Clients.MvcAngular.Models
 {
     public class SampleValue
     {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public SampleEvent Created { get; set; }
+    }
+
+    public class SampleEvent
+    {
+        public SamplePerson By { get; set; }
+        public DateTime At { get; set; }
+    }
+
+    public class SamplePerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
     }
 }
