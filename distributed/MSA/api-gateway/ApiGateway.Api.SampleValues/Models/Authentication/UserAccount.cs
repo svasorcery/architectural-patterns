2c95d640a1658f2a78ab5440c5e1190a498c48eb
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-21 13:29:52 +0300

    added API Authentication models

---
+++ distributed/MSA/api-gateway/ApiGateway.Api.SampleValues/Models/Authentication/UserAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiGateway.Api.SampleValues.Models.Authentication
+{
+    public class UserAccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
