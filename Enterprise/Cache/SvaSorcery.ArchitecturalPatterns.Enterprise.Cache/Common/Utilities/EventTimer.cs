bc9c6350f6aa3c46a450bad54c41400c28a3b872
svasorcery <sva.sorcery@gmail.com>
2020-07-13 19:06:32 +0300

    Enterprise.Cache : Add CollectedCache, closes #28

---
+++ Enterprise/Cache/SvaSorcery.ArchitecturalPatterns.Enterprise.Cache/Common/Utilities/EventTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+
+namespace SvaSorcery.ArchitecturalPatterns.Enterprise.Cache.Common.Utilities
+{
+    public class EventTimer
+    {
+        private readonly Timer _timer;
+
+        public EventTimer(TimeSpan span)
+        {
+            _timer = new Timer
+            {
+                Interval = span.Milliseconds
+            };
+        }
+
+        public void Reset()
+        {
+            _timer.Stop();
+            _timer.Start();
+        }
+
+        public void Start() => _timer.Start();
+        public void Stop() => _timer.Stop();
+
+        public void AddListener(ElapsedEventHandler handler) => _timer.Elapsed += handler;
+        public void RemoveListener(ElapsedEventHandler handler) => _timer.Elapsed -= handler;
+    }
+}
