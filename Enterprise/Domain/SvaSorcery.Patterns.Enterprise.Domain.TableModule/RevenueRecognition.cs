d57122133162b88c010a65e78e36b73f015aaa82
svasorcery <sva.sorcery@gmail.com>
2020-07-15 18:38:38 +0300

    Enterprise.Domain: Add TableModule, closes #33

---
+++ Enterprise/Domain/SvaSorcery.Patterns.Enterprise.Domain.TableModule/RevenueRecognition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace SvaSorcery.Patterns.Enterprise.Domain.TableModule
+{
+    public class RevenueRecognition : TableModule
+    {
+        public RevenueRecognition(DataSet ds) : base(ds, "RevenueRecognitions")
+        {
+        }
+
+        public void Insert(long contractId, decimal amount, DateTime recognizedAt)
+        {
+            DataRow newRow = Table.NewRow();
+            newRow["contractId"] = contractId;
+            newRow["amount"] = amount;
+            newRow["recognizedAt"] = string.Format("{0:s}", recognizedAt);
+            Table.Rows.Add(newRow);
+        }
+
+        public decimal RecognizedRevenue(long contractId, DateTime recognizedAt)
+        {
+            string filter = string.Format("contractId = {0} AND recognizedAt <= #{1:d}#", contractId, recognizedAt);
+            var rows = Table.Select(filter);
+            return rows.Sum(row => (decimal)row["amount"]);
+        }
+    }
+}
