907d45f30393d4687aa226af9f9ae5c022d281a1
svasorcery <sva.sorcery@gmail.com>
2020-07-16 17:01:47 +0300

    Enterprise.DataAccess : Add DataMappery, closes #38

---
+++ Enterprise/DataAccess/SvaSorcery.Patterns.Enterprise.DataAccess.DataMapper/Email.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Linq;
+
+namespace SvaSorcery.Patterns.Enterprise.DataAccess.DataMapper
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+
+        protected static DataTable Table;
+
+        public Email()
+        {
+            Table = new DataSet().Tables["EMAILS"];
+        }
+
+        public static Email GetById(int id)
+            => Table.Select($"ID = {id}")
+                    .Select(x => Map(x))
+                    .First();
+
+        public void Create()
+            => Table.Rows.Add(Map(this));
+
+        public void Update()
+        {
+            var row = Table.Select($"ID = {Id}").First();
+            row.BeginEdit();
+            row = Map(this);
+            row.EndEdit();
+        }
+
+        protected static Email Map(DataRow row)
+            => new Email
+            {
+                Id = (int)row["ID"],
+                Mail = (string)row["MAIL"],
+            };
+
+        protected static DataRow Map(Email model)
+        {
+            var newRow = Table.NewRow();
+            newRow["ID"] = model.Id;
+            newRow["MAIL"] = model.Mail;
+            return newRow;
+        }
+    }
+}
