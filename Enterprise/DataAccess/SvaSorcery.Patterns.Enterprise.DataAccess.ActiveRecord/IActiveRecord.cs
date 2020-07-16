cebbcb1d33d4c57fc81227eaf1a1c3fbe2c3614e
svasorcery <sva.sorcery@gmail.com>
2020-07-16 17:09:05 +0300

    Enterprise.DataAccess : Add ActiveRecord, closes #37

---
+++ Enterprise/DataAccess/SvaSorcery.Patterns.Enterprise.DataAccess.ActiveRecord/IActiveRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace SvaSorcery.Patterns.Enterprise.DataAccess.ActiveRecord
+{
+    public interface IActiveRecord
+    {
+        string TableName { get; }
+        void Create();
+        void Update();
+        void Delete();
+        string ToString();
+    }
+}
