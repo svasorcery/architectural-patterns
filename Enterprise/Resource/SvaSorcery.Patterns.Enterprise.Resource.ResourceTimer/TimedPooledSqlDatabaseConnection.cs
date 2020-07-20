afba8ed709f1ce38a4c498245c4fb1cc967e30df
svasorcery <sva.sorcery@gmail.com>
2020-07-20 13:15:44 +0300

    Enterprise.Resource : Add ResourceTimer, closes #44

---
+++ Enterprise/Resource/SvaSorcery.Patterns.Enterprise.Resource.ResourceTimer/TimedPooledSqlDatabaseConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+using System.Data.SqlClient;
+using SvaSorcery.Patterns.Enterprise.Resource.ResourcePool;
+
+namespace SvaSorcery.Patterns.Enterprise.Resource.ResourceTimer
+{
+    public class TimedPooledSqlDatabaseConnection : DatabaseResource<SqlConnection>
+    {
+        private readonly IResourcePool<DatabaseResource<SqlConnection>> _connectionPool;
+        private DatabaseResource<SqlConnection> _resource;
+        private readonly DatabaseConnectionTimer _timer;
+
+        public TimedPooledSqlDatabaseConnection(IResourcePool<DatabaseResource<SqlConnection>> connectionPool, int timerSeconds)
+            : this(connectionPool, new TimeSpan(0, 0, timerSeconds))
+        {
+        }
+
+        public TimedPooledSqlDatabaseConnection(IResourcePool<DatabaseResource<SqlConnection>> connectionPool, TimeSpan timerSpan)
+        {
+            _connectionPool = connectionPool;
+            _timer = new DatabaseConnectionTimer(timerSpan);
+            _timer.AddListener(OnTimedEvent);
+        }
+
+        public override void Open()
+        {
+            _resource = _connectionPool.GetResource();
+            _timer.Start();
+        }
+
+        public override void Close()
+        {
+            _timer.Stop();
+            _connectionPool.PutResource(_resource);
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e) => Close();
+    }
+}
