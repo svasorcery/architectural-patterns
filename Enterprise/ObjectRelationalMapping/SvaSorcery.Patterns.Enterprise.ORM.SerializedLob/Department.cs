3031e30d06550260d47519747f4753dac0f844f3
svasorcery <sva.sorcery@gmail.com>
2020-07-22 17:03:08 +0300

    Enterprise.ObjectRelationalMapping : Add SerializedLob, closes #53

---
+++ Enterprise/ObjectRelationalMapping/SvaSorcery.Patterns.Enterprise.ORM.SerializedLob/Department.cs
@@ -0,0 +1,38 @@
+﻿namespace SvaSorcery.Patterns.Enterprise.ORM.SerializedLob
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+
+        public virtual Department Parent { get; set; }
+
+        public override string ToString() => ToString(this);
+
+        private string ToString(Department dept)
+        {
+            var value = "";
+            if (dept.Parent is null)
+                return dept.Name;
+
+            value += $"{ToString(dept.Parent)} > {dept.Name}";
+            return value;
+        }
+
+        public Department(int id, string name, int? parentId = null)
+        {
+            Id = id;
+            Name = name;
+            ParentId = parentId;
+        }
+
+        public Department(int id, string name, Department parent)
+        {
+            Id = id;
+            Name = name;
+            ParentId = parent.Id;
+            Parent = parent;
+        }
+    }
+}
