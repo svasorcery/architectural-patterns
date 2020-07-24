7d429c203e9fe06c3154603e3e6bb4507b7f5ca1
svasorcery <sva.sorcery@gmail.com>
2020-07-24 19:16:58 +0300

    Enterprise.InputOutput : Add PagingIterator, closes #58

---
+++ Enterprise/InputOutput/VladimirSva.EnterprisePatterns.PagingIterator/GithubSearchPagedIterator.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using SvaSorcery.Patterns.Enterprise.InputOutput.PagingIterator.Types;
+
+namespace SvaSorcery.Patterns.Enterprise.InputOutput.PagingIterator
+{
+    public class GithubSearchPagedIterator : AbstractPagedIterator<object>
+    {
+        private readonly string _baseUrl = "https://api.github.com/search/repositories";
+        private readonly HttpClient _httpClient;
+        private int _totalSize;
+        private string _term;
+
+        public GithubSearchPagedIterator(string term)
+        {
+            _httpClient = new HttpClient();
+            _term = term;
+            _totalSize = 0;
+            GetPage(0);
+        }
+
+        public override int GetTotalSize() => _totalSize;
+
+        public override int GetPageSize() => 100;
+
+        public override object[] GetPage(int pageNumber)
+        {
+            var url = $"{_baseUrl}?q={_term}&page={pageNumber}";
+
+            var response = _httpClient.GetStringAsync(url).Result;
+
+            var result = JsonConvert.DeserializeObject<GithubResult>(response);
+
+            _totalSize = result.TotalCount;
+
+            return result.Items;
+        }
+    }
+
+    public class GithubResult
+    {
+        public int TotalCount { get; set; }
+        public object[] Items { get; set; }
+    }
+}
---
+++ Enterprise/InputOutput/VladimirSva.EnterprisePatterns.PagingIterator/Types/AbstractPagedIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SvaSorcery.Patterns.Enterprise.InputOutput.PagingIterator.Types
+{
+    public abstract class AbstractPagedIterator<T>
+    {
+        public abstract int GetPageSize();
+        public abstract int GetTotalSize();
+        public abstract T[] GetPage(int pageNumber);
+
+        public bool UseCache => true;
+        public IDictionary<int, T[]> CachedPages { get; set; }
+        public T[] CurrentPage { get; set; }
+        public int CurrentPageNumber { get; set; }
+        public int Index { get; set; } = 0;
+
+        public T Current => GetOffset(Index);
+        public int Count => GetTotalSize();
+        public bool IsValid => OffsetExists(Index);
+        public bool OffsetExists(int offset) => offset >= 0 && offset < Count;
+        public void Next() => ++Index;
+        public void Rewind() => Index = 0;
+
+        public T GetOffset(int offset)
+        {
+            if (!OffsetExists(offset))
+                throw new ArgumentOutOfRangeException(nameof(offset));
+
+            var page = offset / GetPageSize();
+
+            if (UseCache)
+            {
+                if (!CachedPages.Keys.Contains(page))
+                {
+                    CachedPages[page] = GetPage(page);
+                }
+                return CachedPages[page][offset % GetPageSize()];
+            }
+
+            if (page != CurrentPageNumber)
+            {
+                CurrentPageNumber = page;
+                CurrentPage = GetPage(page);
+            }
+            return CurrentPage[offset % GetPageSize()];
+        }
+    }
+}
