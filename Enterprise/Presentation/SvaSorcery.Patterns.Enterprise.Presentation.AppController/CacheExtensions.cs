19d5cac0ed881bf7ef911bbbfb6b900998818362
svasorcery <sva.sorcery@gmail.com>
2020-07-28 19:31:04 +0300

    Enterprise.Presentation : Add ApplicationController, closes #64

---
+++ Enterprise/Presentation/SvaSorcery.Patterns.Enterprise.Presentation.AppController/CacheExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SvaSorcery.Patterns.Enterprise.Cache;
+using SvaSorcery.Patterns.Enterprise.Cache.Common.Types;
+using SvaSorcery.Patterns.Enterprise.Cache.Common.Utilities;
+using SvaSorcery.Patterns.Enterprise.Presentation.AppController.Models;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CacheExtensions
+    {
+        public static void AddCache(this IServiceCollection services)
+        {
+            services.AddScoped<CacheAccessor<Person>>();
+            services.AddScoped<DemandCache<Person>>();
+            services.AddScoped<CacheReplicator<Person>>();
+            services.AddScoped<ICache<Person>, ReplicatedCache<Person>>();
+        }
+    }
+}
---
+++ Enterprise/Presentation/SvaSorcery.Patterns.Enterprise.Presentation.AppController/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using SvaSorcery.Patterns.Enterprise.Cache.Common.Types;
+
+namespace SvaSorcery.Patterns.Enterprise.Presentation.AppController.Models
+{
+    public class Person : IIdentifiable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
