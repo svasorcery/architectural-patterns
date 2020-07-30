b0929bdbacce8660293e9815de9d97b5e640fdc6
svasorcery <sva.sorcery@gmail.com>
2020-07-30 18:42:18 +0300

    Enterprise.Session : Add DatabaseSession, closes #69

---
+++ Enterprise/Session/SvaSorcery.Patterns.Enterprise.Session.DatabaseSession/Persistence/IMongoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace SvaSorcery.Patterns.Enterprise.Session.DatabaseSession.Persistence
+{
+    public interface IMongoRepository<TEntity> where TEntity : IIdentifiable
+    {
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetAsync(Guid id);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+    }
+
+    public interface IIdentifiable
+    {
+        public Guid Id { get; set; }
+    }
+}
