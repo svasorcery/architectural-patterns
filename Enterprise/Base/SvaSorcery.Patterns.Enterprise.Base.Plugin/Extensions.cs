07452d42fb16bb0a612a5925c11040d285ec5cf0
svasorcery <sva.sorcery@gmail.com>
2020-08-05 18:33:43 +0300

    Enterprise.Base : Add Plugin, closes #78

---
+++ Enterprise/Base/SvaSorcery.Patterns.Enterprise.Base.Plugin/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using SvaSorcery.Patterns.Enterprise.Base.Plugin;
+using SvaSorcery.Patterns.Enterprise.Base.SpecialCase;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class Extensions
+    {
+        public static void AddPersonPlugin(this IServiceCollection services)
+        {
+            services.AddScoped<IPersonExtensions, HttpContextUserPersonPlugin>();
+        }
+    }
+}
---
+++ Enterprise/Base/SvaSorcery.Patterns.Enterprise.Base.Plugin/HttpContextUserPersonPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using SvaSorcery.Patterns.Enterprise.Base.SpecialCase;
+using SvaSorcery.Patterns.Enterprise.DataAccess.TableDataGateway;
+
+namespace SvaSorcery.Patterns.Enterprise.Base.Plugin
+{
+    public class HttpContextUserPersonPlugin : IPersonExtensions
+    {
+        private HttpContext _httpContext;
+
+        public HttpContextUserPersonPlugin(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContext = httpContextAccessor.HttpContext;
+        }
+
+        public Person RepositoryOwner()
+            => new Person()
+            {
+                Id = int.Parse(_httpContext.User.Claims.FirstOrDefault(x => x.Type == "id")?.Value),
+                FirstName = _httpContext.User.Identity.Name.Split(' ')[0],
+                LastName = _httpContext.User.Identity.Name.Split(' ')[1],
+                Email = _httpContext.User.Claims.FirstOrDefault(x => x.Type == "email")?.Value
+            };
+
+        public Person NullPerson()
+            => new Person()
+            {
+                Id = 0,
+                FirstName = null,
+                LastName = null,
+                Email = null
+            };
+    }
+}
