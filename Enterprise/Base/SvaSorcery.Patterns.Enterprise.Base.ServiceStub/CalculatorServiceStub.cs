c526f214795a4898c6efaaaa193bb85620844c7d
svasorcery <sva.sorcery@gmail.com>
2020-08-05 18:37:04 +0300

    Enterprise.Base : Add ServiceStub, closes #79

---
+++ Enterprise/Base/SvaSorcery.Patterns.Enterprise.Base.ServiceStub/CalculatorServiceStub.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace SvaSorcery.Patterns.Enterprise.Base.ServiceStub
+{
+    public class CalculatorServiceStub : ICalculatorService
+    {
+        public Task<int> AddAsync(int a, int b) => Task.FromResult(a + b);
+
+        public Task<int> SubtractAsync(int a, int b) => Task.FromResult(a - b);
+
+        public Task<int> MultiplyAsync(int a, int b) => Task.FromResult(a * b);
+
+        public Task<int> DivideAsync(int a, int b) => Task.FromResult(a / b);
+    }
+}
---
+++ Enterprise/Base/SvaSorcery.Patterns.Enterprise.Base.ServiceStub/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SvaSorcery.Patterns.Enterprise.Base.ServiceStub
+{
+    public static class Extensions
+    {
+        public static void AddCalculatorService(this IServiceCollection services)
+        {
+            services.AddScoped<ICalculatorService, CalculatorServiceStub>();
+        }
+    }
+}
